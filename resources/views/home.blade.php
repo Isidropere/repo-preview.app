225b20ff53cd235c29c7abdfc804d2b2c0ad00c7
Cambialo Dev <dev@cambialo.rd>
2026-06-12 22:28:06 -0400

    style: fix bottom ad carousel layout by adding h-full to slides and defining responsive heights

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -73,6 +73,21 @@
         padding-bottom: 2.5rem !important; /* 40px */
     }
 }
+
+/* Bottom Ad Carousel Height */
+#home-ad-carousel {
+    height: 150px !important;
+}
+@media (min-width: 640px) {
+    #home-ad-carousel {
+        height: 220px !important;
+    }
+}
+@media (min-width: 1024px) {
+    #home-ad-carousel {
+        height: 320px !important;
+    }
+}
 </style>
 @endpush
 
@@ -296,12 +311,12 @@
         &quot;loadingClasses&quot;: &quot;opacity-0&quot;,
        &quot;isAutoPlay&quot;: &quot;true&quot;
       }" class="relative ">
-                    <div class="hs-carousel relative overflow-hidden w-full min-h-[250px] lg:min-h-[250px] bg-white ">
+                    <div id="home-ad-carousel" class="hs-carousel relative overflow-hidden w-full min-h-[250px] lg:min-h-[250px] bg-white">
                         <div class="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
-                            <div class="hs-carousel-slide">
+                            <div class="hs-carousel-slide h-full">
                             <img src="/imgs/1.jpg" class="h-full w-full object-cover" alt="Banner publicitario Cambialord" width="1200" height="530"> </div>
-                            <div class="hs-carousel-slide"> <img src="/imgs/2.jpg" class="h-full w-full object-cover" alt="Promociones Cambialord" width="1200" height="530"> </div>
-                            <div class="hs-carousel-slide"> <img src="/imgs/3.jpg" class="h-full w-full object-cover" alt="Descubre productos en Cambialord" width="1200" height="530"> </div>
+                            <div class="hs-carousel-slide h-full"> <img src="/imgs/2.jpg" class="h-full w-full object-cover" alt="Promociones Cambialord" width="1200" height="530"> </div>
+                            <div class="hs-carousel-slide h-full"> <img src="/imgs/3.jpg" class="h-full w-full object-cover" alt="Descubre productos en Cambialord" width="1200" height="530"> </div>
                         </div>
                     </div> <button type="button" class="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 "> <span class="text-2xl" aria-hidden="true"> <svg class="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <path d="m15 18-6-6 6-6"></path> </svg> </span> <span class="sr-only">Previous</span> </button>                    <button type="button" class="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 "> <span class="sr-only">Next</span> <span class="text-2xl" aria-hidden="true"> <svg class="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <path d="m9 18 6-6-6-6"></path> </svg> </span> </button>
                     <div class="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 space-x-2"> <span class="hs-carousel-active:bg-primary hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer"></span> <span class="hs-carousel-active:bg-primary hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer"></span>                        <span class="hs-carousel-active:bg-primary hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer"></span> </div>
